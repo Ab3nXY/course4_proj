9fcfd08336621ea6c36af01a938a760a3b04cffb
Abenezer Shiferaw <abenxy0@gmail.com>
2023-11-02 18:25:37 +0000

    Finish celery introduction

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -41,12 +41,16 @@ class Dev(Configuration):
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SAMESITE = "None"
     SESSION_COOKIE_SAMESITE = "None"
+    CELERY_RESULT_BACKEND = "django-db"
+    CELERY_BROKER_URL = "redis://localhost:6379/0"
+
 
 
 
     # Application definition
 
     INSTALLED_APPS = [
+        'django_celery_results',
         'movies',
         "gh",
         'django.contrib.admin',
